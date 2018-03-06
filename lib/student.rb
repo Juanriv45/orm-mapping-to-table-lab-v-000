68dcd354bdbf76d75080932d22a408616d1c38b1
Juan Rivera <juanriv45@gmail.com>
2018-03-06 03:56:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,7 @@ class Student
     @id = id
   end
   def self.create_table
-    sql = <<- SQL
+    sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
